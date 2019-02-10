f9b6400976afd5dfbe61b1a83004eaff2f9b8b5f
Yuze Jiang <fortissimoX@gmail.com>
2019-02-10 13:45:14 -0600

    Rename class

--- mpv-iina.rb
+++ mpv-iina.rb
@@ -1,4 +1,4 @@
-class Mpv < Formula
+class MpvIina < Formula
   desc "Media player based on MPlayer and mplayer2"
   homepage "https://mpv.io"
   url "https://github.com/mpv-player/mpv/archive/v0.29.1.tar.gz"
