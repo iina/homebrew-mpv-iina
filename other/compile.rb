44ba1c7391a35fd122fd7da0aac2da29d39808c3
Yuze Jiang <fortissimoX@gmail.com>
2019-05-13 23:57:16 -0500

    Ensure cleaning

--- other/compile.rb
+++ other/compile.rb
@@ -23,25 +23,26 @@ def patch_python
   File.open(file_path, 'w') { |file| file.write lines.join }
 end
 
-if compile_deps
-  homebrew_path = "#{`brew --prefix`.chomp}/Homebrew/"
-  FileUtils.cd homebrew_path
-  system "git reset --hard HEAD"
-  print "Applying Homebrew patch (MACOSX_DEPLOYMENT_TARGET)\n"
-  system "git apply #{current_dir}/#{homebrew_patch}"
-  deps = "#{`brew deps mpv-iina -n`}".split("\n")
-  print "#{deps.length + 1} packages to be complied\n"
+begin
+  if compile_deps
+    homebrew_path = "#{`brew --prefix`.chomp}/Homebrew/"
+    FileUtils.cd homebrew_path
+    system "git reset --hard HEAD"
+    print "Applying Homebrew patch (MACOSX_DEPLOYMENT_TARGET)\n"
+    system "git apply #{current_dir}/#{homebrew_patch}"
+    deps = "#{`brew deps mpv-iina -n`}".split("\n")
+    print "#{deps.length + 1} packages to be complied\n"
 
-  deps.each do |dep|
-    install dep
-    if dep == "python"
-      patch_python
+    deps.each do |dep|
+      install dep
+      if dep == "python"
+        patch_python
+      end
     end
   end
-end
 
-install "mpv-iina"
+  install "mpv-iina"
 
-at_exit {
+ensure
   system "git reset --hard HEAD" if compile_deps
-}
+end
