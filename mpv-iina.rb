bddf333b9bd55c09a7cc36c684edea2e582fe3a5
Yuze Jiang <fortissimoX@gmail.com>
2020-02-25 21:15:42 -0600

    Update mpv to 0.32.0

--- mpv-iina.rb
+++ mpv-iina.rb
@@ -1,8 +1,8 @@
 class MpvIina < Formula
   desc "Media player based on MPlayer and mplayer2"
   homepage "https://mpv.io"
-  url "https://github.com/mpv-player/mpv/archive/v0.31.0.tar.gz"
-  sha256 "805a3ac8cf51bfdea6087a6480c18835101da0355c8e469b6d488a1e290585a5"
+  url "https://github.com/mpv-player/mpv/archive/v0.32.0.tar.gz"
+  sha256 "9163f64832226d22e24bbc4874ebd6ac02372cd717bef15c28a0aa858c5fe592"
   head "https://github.com/mpv-player/mpv.git"
 
   keg_only "it is intended to only be used for building IINA. This formula is not recommended for daily use"
