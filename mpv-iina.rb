013c2ea31abfc6bb499d74796eeaeae4cf89debc
Yuze Jiang <i@uiryuu.moe>
2023-03-22 22:46:36 +0800

    Update mpv-iina.rb; upgrade mpv to 0.35.1
    Cherrypick decisions:
    bbb2076ea46d96d7f5b98b33232a84ac93c86939: dropped, already depends on yt-dlp
    0e124c8aae38dad19a5470b905840bb410fa6eb1: partially picked. Since we always build on the latest macOS version, dropped the macOS SDK detection part; all other changes are picked.
    c894ef135f02d055d9bed8adf5980577ed224ac8: picked. Building tool changed from waf to meson, python is not a dependency anymore
    99fb896882d3fc109395bb439fe9f019dbb73569: picked
    
    Fix #17

--- mpv-iina.rb
+++ mpv-iina.rb
@@ -4,23 +4,23 @@
 class MpvIina < Formula
   desc "Media player based on MPlayer and mplayer2"
   homepage "https://mpv.io"
-  url "https://github.com/mpv-player/mpv/archive/v0.34.1.tar.gz"
-  sha256 "32ded8c13b6398310fa27767378193dc1db6d78b006b70dbcbd3123a1445e746"
+  url "https://github.com/mpv-player/mpv/archive/v0.35.1.tar.gz"
+  sha256 "41df981b7b84e33a2ef4478aaf81d6f4f5c8b9cd2c0d337ac142fc20b387d1a9"
   head "https://github.com/mpv-player/mpv.git"
 
   keg_only "it is intended to only be used for building IINA. This formula is not recommended for daily use"
 
   depends_on "docutils" => :build
-  depends_on "pkg-config" => :build
-  depends_on "python@3.9" => :build
+  depends_on "meson" => :build
+  depends_on "pkg-config" => [:build, :test]
   depends_on xcode: :build
 
   depends_on "ffmpeg-iina"
-  depends_on "jpeg"
+  depends_on "jpeg-turbo"
   depends_on "libarchive"
   depends_on "libass"
   depends_on "little-cms2"
-  depends_on "luajit-openresty"
+  depends_on "luajit"
   depends_on "libbluray"
 
   depends_on "mujs"
@@ -34,35 +34,51 @@ class MpvIina < Formula
     # that's good enough for building the manpage.
     ENV["LC_ALL"] = "C"
 
+    # force meson find ninja from homebrew
+    ENV["NINJA"] = Formula["ninja"].opt_bin/"ninja"
+
     # libarchive is keg-only
     ENV.prepend_path "PKG_CONFIG_PATH", Formula["libarchive"].opt_lib/"pkgconfig"
-    # luajit-openresty is key-only
-    ENV.prepend_path "PKG_CONFIG_PATH", Formula["luajit-openresty"].opt_lib/"pkgconfig"
 
     args = %W[
-      --prefix=#{prefix}
-      --enable-javascript
-      --enable-libmpv-shared
-      --enable-lua
-      --enable-libarchive
-      --enable-uchardet
-      --enable-libbluray
-      --disable-swift
-      --disable-debug-build
-      --disable-macos-media-player
-      --confdir=#{etc}/mpv
+      -Dhtml-build=disabled
+      -Djavascript=enabled
+      -Dlibmpv=true
+      -Dlua=luajit
+      -Dlibarchive=enabled
+      -Duchardet=enabled
+
+      -Dlibbluray=enabled
+      -Dcplayer=false
+
+      -Dswift-build=disabled
+      -Dmacos-cocoa-cb=disabled
+      -Dmacos-cocoa-media-player=disabled
+      -Dmacos-touchbar=disabled
+      -Dmanpage-build=disabled
+
+      --sysconfdir=#{pkgetc}
       --datadir=#{pkgshare}
-      --mandir=#{man}
-      --docdir=#{doc}
-      --lua=luajit
     ]
 
-    system Formula["python@3.9"].opt_bin/"python3", "bootstrap.py"
-    system Formula["python@3.9"].opt_bin/"python3", "waf", "configure", *args
-    system Formula["python@3.9"].opt_bin/"python3", "waf", "install"
+    system "meson", "setup", "build", *args, *std_meson_args
+    system "meson", "compile", "-C", "build", "--verbose"
+    system "meson", "install", "-C", "build"
+
+    # `pkg-config --libs mpv` includes libarchive, but that package is
+    # keg-only so it needs to look for the pkgconfig file in libarchive's opt
+    # path.
+    libarchive = Formula["libarchive"].opt_prefix
+    inreplace lib/"pkgconfig/mpv.pc" do |s|
+     s.gsub!(/^Requires\.private:(.*)\blibarchive\b(.*?)(,.*)?$/,
+             "Requires.private:\\1#{libarchive}/lib/pkgconfig/libarchive.pc\\3")
+    end
+
   end
 
   test do
     system bin/"mpv", "--ao=null", test_fixtures("test.wav")
+    # Make sure `pkg-config` can parse `mpv.pc` after the `inreplace`.
+    system "pkg-config", "mpv"
   end
 end
